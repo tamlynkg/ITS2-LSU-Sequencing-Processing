a0387bc5db944c21e23160b0ed0de9381d838f54
Tamlyn Gangiah <50321782+tamlynkg@users.noreply.github.com>
2024-08-21 15:23:43 +0200

    Update process_data.R

--- process_data.R
+++ process_data.R
@@ -7,8 +7,38 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
 BiocManager::install("Biostrings")
 BiocManager::install("dplyr")
 BiocManager::install("argparse")
+BiocManager::install("tidyr")
 library(Biostrings)
 library(dplyr)
+library(tidyr)
+
+# Read FASTA file
+read_fasta_file <- function(fasta_file) {
+  sequences <- readDNAStringSet(fasta_file)
+#  new_headers <- sub("_repseq_0", "", names(sequences))
+#  names(sequences) <- new_headers
+  return(sequences)
+}
+
+fasta_file <- snakemake@input[['fasta_file']]
+
+sequences <- read_fasta_file(fasta_file) 
+
+#Getting ITS Length 
+
+#ITSx_positions <- snakemake@input[['ITSx_positions]]
+
+df <- read.delim("Atnarova.positions.txt", header = FALSE)
+
+colnames(df) <- c("scata_ref", "length", "SSU", "ITS1", "5.8S", "ITS2", "LSU", "sequence")
+
+#so ITSx positions only includes fungal
+df$scata_ref <- gsub("_repseq_0", "", df$scata_ref)
+df <- df[df$scata_ref %in% names(sequences), ]
+
+#Remove reads where a LSU is not found
+df <- df %>%
+  filter(LSU != "LSU: Not found")
 
 read_data_and_labels <- function(data_file, labels_file) {
   data <- read.delim(data_file, header = TRUE, sep = ";")
@@ -19,65 +49,44 @@ read_data_and_labels <- function(data_file, labels_file) {
 }
 
 
-fasta_file <- snakemake@input[['fasta_file']]
 labels_file <- snakemake@input[['labels_file']]
-
 
 data_file <- "data/all_tag_by_cluster_counts.txt"
  
-
 data <- read_data_and_labels(data_file, labels_file)
 
 dataunfiltered <- data
 
-# Read FASTA file
-read_fasta_file <- function(fasta_file) {
-  sequences <- readDNAStringSet(fasta_file)
-  new_headers <- sub("_repseq_0", "", names(sequences))
-  names(sequences) <- new_headers
-  return(sequences)
-}
+labels <- read.delim(labels_file)
 
-sequences <- read_fasta_file(fasta_file) 
+filtered.counts.data <- data[,-1]
+filtered.counts.data <- filtered.counts.data[, colSums(filtered.counts.data) != 0]
+filtered.counts.data$Sample <- labels$Sample
+filtered.counts.data$Sample <- trimws(filtered.counts.data$Sample)
 
-# Remove columns not present in FASTA file
+
+# Remove columns not present in FASTA file as they are non-fungal
 remove_columns_not_in_fasta <- function(data, sequences) {
   columns_to_remove <- setdiff(names(data), names(sequences))
   data <- data[, !names(data) %in% columns_to_remove]
+  data <- data[, names(data) %in% df$scata_ref]
   return(data)
 }
 
-data <- remove_columns_not_in_fasta(data, sequences)
+filtered.counts.data <- remove_columns_not_in_fasta(filtered.counts.data, sequences)
 
-#proportion of data that was non-fungal
-#reads_lost <- (sum(dataunfiltered[,-1])-sum(data))/sum(dataunfiltered[,-1])*100
+matching_sequences <- sequences[names(sequences) %in% names(filtered.counts.data)]
 
-#Calculate relative abundances
-calculate_rel_abundance <- function(data) {
-  sample_totals <- rowSums(data)
-  data <- data / sample_totals
-  return(data)
-}
+# Write matching sequences to a new FASTA file
+output_fasta_file <- "source_11.fasta"
+writeXStringSet(matching_sequences, file=output_fasta_file)
 
-data <- calculate_rel_abundance(data)
+filtered.counts.data$Sample <- labels$Sample
+filtered.counts.data$Sample <- substr(filtered.counts.data$Sample, 1, 3)
+filtered.counts.data <- aggregate(. ~ Sample, filtered.counts.data, FUN = sum)
 
-# Function to process a row 
-process_row <- function(row) {
-  sorted_row <- sort(row, decreasing = TRUE)  # Sort the row in descending order
-  cumsum_row <- cumsum(sorted_row)  # Calculate the cumulative sum
-  index <- which.max(cumsum_row >= 0.80)  # Find the index where the cumulative sum first reaches 90% or more
-  selected_columns <- names(sorted_row)[1:index]   # Select columns up to the identified index
-  return(selected_columns)
-}
+write.csv(filtered.counts.data, "countsdata_filtered.csv")
 
-# Process rows and write matching sequences to a new FASTA file
-process_rows_and_write_fasta <- function(data, sequences, fasta_file) {
-  result <- apply(data[, -1], 1, process_row) #Apply the process_row function to each row in the data frame
-  unique_values <- unique(unlist(result)) # Combine and filter unique values
-  matching_sequences <- sequences[names(sequences) %in% unique_values]
-  writeXStringSet(matching_sequences, file = fasta_file) # Write matching sequences to a new FASTA file
-}
 
-write.csv(data, file = "processed_data.csv", row.names = FALSE)
-finalfasta <- "80relabundITS.fasta"
-process_rows_and_write_fasta(data, sequences, finalfasta)
+
+
